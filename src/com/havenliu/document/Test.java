541a014d6fc59c46cb9338738f025f07a36aa2fa
wuqianggithub <wuqiangjobs@163.com>
2016-05-05 14:52:09 +0800

    Test

---
+++ src/com/havenliu/document/Test.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:Document
+ * File Name:Test.java
+ * Package Name:com.havenliu.document
+ * Date:2016年5月5日下午2:51:52
+ * Copyright (c) 2016, chenzhou1025@126.com All Rights Reserved.
+ *
+*/
+
+package com.havenliu.document;
+/**
+ * ClassName:Test <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2016年5月5日 下午2:51:52 <br/>
+ * @author   Administrator
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class Test {
+
+}
+
